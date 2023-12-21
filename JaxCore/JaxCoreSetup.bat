af65f54795c94bdadaf040bd61dd4d7d8c5a83ab
StillSlow <89484696+oxiox24@users.noreply.github.com>
2023-12-21 02:17:54 +0000

    FIX

--- JaxCore/JaxCoreSetup.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-cmd /c start powershell -ExecutionPolicy Bypass -command "IWR -UseBasicParsing "https://raw.githubusercontent.com/Jax-Core/JaxCore/master/CoreInstaller.ps1" | IEX"
--- v2.py
+++ v2.py
@@ -22,8 +22,7 @@ def button2():
 def button3():
     print('installing...')
     abutton.configure(state='disabled',text="Installing...")
-    os.chdir('JaxCore')
-    msg = subprocess.run(['JaxCoreSetup.bat'])
+    msg = subprocess.run(['winget', 'install', 'BeWidgets'])
     print(f'{msg}')
     time.sleep(1)
     abutton.configure(text="Installed")
@@ -47,8 +46,8 @@ button1 = customtkinter.CTkButton(master=instance, text='install',command=button
 button1.place(relx=0.88, rely=0.228, anchor='center')
 
 
-JaxCore = customtkinter.CTkLabel(master=instance,text="JaxCore (Latest version(Disable AV))",font=('Montserrat Ultra-Bold',18))
-JaxCore.place(relx=0.22,rely=0.328, anchor=customtkinter.CENTER)
+BeWidgets = customtkinter.CTkLabel(master=instance,text="BeWidgets (Latest version)",font=('Montserrat Ultra-Bold',18))
+BeWidgets.place(relx=0.17,rely=0.328, anchor=customtkinter.CENTER)
 abutton = customtkinter.CTkButton(master=instance, text='install',command=button3)
 abutton.place(relx=0.88, rely=0.328, anchor='center')
 instance.resizable(False,False)
